9e299df73f84f42614fd60b5016040ae030386aa
Alex Mostovnikov <mostovnikov.alex@gmail.com>
2020-02-13 20:25:47 +0300

    Initiate new project

---
+++ NewsHub/NewsHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsHub
+//
+//  Created by Alex Mostovnikov on 13/2/20.
+//  Copyright © 2020 MALX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
