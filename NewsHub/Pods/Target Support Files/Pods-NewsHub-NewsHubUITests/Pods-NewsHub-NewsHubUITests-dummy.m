34c4abd7b5be6e65ab849b531381ef7f722f09cc
Alex Mostovnikov <mostovnikov.alex@gmail.com>
2020-02-16 16:40:07 +0300

    Add HTML and RSS parser to the project,
    Also change minimum support version of iOS to 13.0

---
+++ NewsHub/Pods/Target Support Files/Pods-NewsHub-NewsHubUITests/Pods-NewsHub-NewsHubUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NewsHub_NewsHubUITests : NSObject
+@end
+@implementation PodsDummy_Pods_NewsHub_NewsHubUITests
+@end
---
+++ NewsHub/Pods/Target Support Files/Pods-NewsHubTests/Pods-NewsHubTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NewsHubTests : NSObject
+@end
+@implementation PodsDummy_Pods_NewsHubTests
+@end
