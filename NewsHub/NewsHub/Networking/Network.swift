d8bf793e490ba9b592bf7ad6c6c900b201685459
Alex Mostovnikov <mostovnikov.alex@gmail.com>
2020-02-25 02:27:24 +0300

    Add Network class and functionality to parse and show all RSS feeds from TUT.BY

---
+++ NewsHub/NewsHub/Networking/Network.swift
@@ -0,0 +1,49 @@
+//
+//  Network.swift
+//  NewsHub
+//
+//  Created by Alex Mostovnikov on 25/2/20.
+//  Copyright © 2020 MALX. All rights reserved.
+//
+
+import Foundation
+
+class Network: NSObject {
+    
+    func getRSSPageOfSite(by url: URL, completion: @escaping (_ htmlDocument: String) -> Void) {
+        let session = URLSession(configuration: .ephemeral)
+        let dataTask = session.dataTask(with: url) { (data, response, error) in
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                completion("")
+                return
+            }
+            
+            if let data = data, let stringData = String(data: data, encoding: .utf8) {
+                completion(stringData)
+                return
+            }
+            completion("")
+        }
+        dataTask.resume()
+    }
+    
+    func detectRssFeed(by url: URL, completion: @escaping (_ isRSS: Bool) -> Void) {
+        let session = URLSession(configuration: .ephemeral)
+        var request = URLRequest(url: url)
+        request.httpMethod = "Head"
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            guard let httpResponse = response as? HTTPURLResponse,
+                httpResponse.statusCode == 200,
+                let contentType = httpResponse.allHeaderFields["Content-Type"] as? String else {
+                completion(false)
+                return
+            }
+            if contentType.hasPrefix("application/rss+xml") {
+                completion(true)
+            } else {
+                completion(false)
+            }
+        }
+        dataTask.resume()
+    }
+}
--- NewsHub/NewsHub/ViewControllers/Home/HomeViewController.swift
+++ NewsHub/NewsHub/ViewControllers/Home/HomeViewController.swift
@@ -7,13 +7,83 @@
 //
 
 import UIKit
+import SwiftSoup
 
 class HomeViewController: UIViewController {
+    @IBOutlet private var tableView: UITableView!
+    
+    private var rssFeeds: [(link: String, title: String)] = []
+    private var cellIdentifier = "DefaultCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "TUT.BY feeds"
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        loadRSSFeeds()
     }
 
+    private func loadRSSFeeds() {
+        let network = Network()
+        let dispatchGroup = DispatchGroup()
+        dispatchGroup.enter()
+        network.getRSSPageOfSite(by: URL(string: "https://news.tut.by/rss.html")!) {[weak self] (htmlDocument) in
+            do {
+                let doc: Document = try SwiftSoup.parse(htmlDocument)
+                let elements = try doc.getAllElements()
+                
+                for element in elements {
+                    if element.hasAttr("href"), let link = try? element.attr("href"), let url = URL(string: link) {
+                        dispatchGroup.enter()
+                        network.detectRssFeed(by: url) {[weak self] (isRSS) in
+                            if isRSS {
+                                self?.rssFeeds.append((link: url.absoluteString, title: try! element.text()))
+                            }
+                            dispatchGroup.leave()
+                        }
+                    }
+                }
+            } catch {
+                
+            }
+            dispatchGroup.leave()
+        }
+        dispatchGroup.notify(queue: .main) { [weak self] in
+            self?.showRssFeeds()
+        }
+    }
+    
+    @IBAction private func onLoadRSS() {
+        loadRSSFeeds()
+    }
+    
+    private func showRssFeeds() {
+        tableView.reloadData()
+    }
 }
 
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rssFeeds.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) {
+            cell.textLabel?.text = rssFeeds[indexPath.row].title
+            cell.accessoryType = .disclosureIndicator
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // TODO: implement functionality to load rss content for selected feed
+        print(rssFeeds[indexPath.row].link)
+    }
+}
