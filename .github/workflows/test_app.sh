552ece3b18a8f1ca749666118efaf649bb9b2b02
Alex Mostovnikov <mostovnikov.alex@gmail.com>
2020-09-12 23:20:03 +0300

    Add testing script for GitHub CI system

---
+++ .github/workflows/test_app.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -eo pipefail
+
+xcodebuild -workspace NewsHub.workspace \
+            -scheme NewsHub\ iOS \
+            -destination platform=iOS\ Simulator,OS=13.4,name=iPhone\ 11 \
+            clean test | xcpretty
