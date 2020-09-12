4b381bddde31d626b61f0d133706a4017b9dd714
Alex Mostovnikov <mostovnikov.alex@gmail.com>
2020-09-13 00:00:40 +0300

    Update test_app.sh

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,6 +4,6 @@
 set -eo pipefail
 
 xcodebuild -workspace NewsHub/NewsHub.xcworkspace \
-            -scheme NewsHub\ iOS \
+            -scheme NewsHub \
             -destination platform=iOS\ Simulator,OS=13.4,name=iPhone\ 11 \
             clean test | xcpretty
